9c2e3b947ae008d80a7b429ff6e6b27aa9859a13
Ken Horn <ken.horn@gmail.com>
2020-04-21 15:11:50 +0100

    adding html starter files

---
+++ code.js
@@ -0,0 +1,37 @@
+
+document.getElementById("page_1_to_2").addEventListener('click', () => { 
+  document.getElementById("page2").style.display = "block";
+  document.getElementById("page1").style.display = "none";
+
+  startTimer();
+})
+
+document.getElementById("page_2_to_1").addEventListener('click', () => { 
+  document.getElementById("page1").style.display = "block";
+  document.getElementById("page2").style.display = "none";
+
+})
+
+
+document.getElementById("cookie").addEventListener('click', () => {
+  if (remaining > 0) {
+      score = score +1;
+  }
+  document.getElementById("score").innerHTML = score;
+  
+})
+
+function startTimer() {
+  score = 0;
+  remaining = 60;
+
+  handle = setInterval(() => {
+    remaining = remaining - 1;
+    document.getElementById("remaining").innerHTML = remaining;
+
+    if (remaining == 0) {
+      clearInterval(handle);
+    }
+  }, 1000)
+
+}
